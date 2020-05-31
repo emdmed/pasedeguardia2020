4f582df193d57dedbb2c647c0c23c31ff40e8508
nevor <nevikhun@gmail.com>
2020-05-31 19:15:03 -0300

    1

--- client/js/service-worker.js
+++ client/js/service-worker.js
@@ -35,17 +35,17 @@ function notifyMe(title) {
     // check if permission is already granted
     if (Notification.permission === 'granted') {
         // show notification here
-        var notify = new Notification('Pendiente!', {
+        self.registration.showNotification("Pendiente!", {
             body: title
-        });
+        })
     } else {
         // request permission from user
         Notification.requestPermission().then(function (p) {
             if (p === 'granted') {
                 // show notification here
-                var notify = new Notification('Pendiente!', {
+                self.registration.showNotification("Pendiente!", {
                     body: title
-                });
+                })
             } else {
                 console.log('User blocked notifications.');
             }
